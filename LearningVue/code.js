ee6203e0bd7a9e726a6938fa903cd2bf9bd3dc83
Jean-Christophe Bourgault <jcbourgault2008@yahoo.ca>
2019-01-13 02:29:10 -0500

    Adding Vue stuff
    Some serious things... some not really. ¯\_(ツ)_/¯

---
+++ LearningVue/code.js
@@ -0,0 +1,55 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    message: "Hahaha penis"
+  }
+});
+
+var app2 = new Vue({
+  el: "#app-2",
+  data: {
+    message: "Page loaded on " + new Date().toLocaleString()
+  }
+});
+
+var app3 = new Vue({
+  el: "#app-3",
+  data: {
+    seen: true
+  }
+});
+
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [{
+        text: 'Learn JavaScript'
+      },
+      {
+        text: 'Learn Vue'
+      },
+      {
+        text: 'Build something awesome'
+      }
+    ]
+  }
+});
+
+var app5 = new Vue({
+  el: "#app-5",
+  data: {
+    message: "Oh, hi!"
+  },
+  methods: {
+    reverseMessage: function(){
+      this.message = "8===D";
+    }
+  }
+});
+
+var app6 = new Vue({
+  el: "#app-6",
+  data: {
+    message: "This is a default message"
+  }
+});
