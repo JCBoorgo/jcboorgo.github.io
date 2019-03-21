88ee8be4f6d10b2ced5e4a539f50ecb6c945916e
Boud <maxime.boudreault.0@gmail.com>
2019-03-21 17:21:12 -0400

    Add NG+

--- ShardSweeper/code.js
+++ ShardSweeper/code.js
@@ -4,35 +4,47 @@ var app = new Vue({
     mines: [],
     clicked: [],
     score: 0,
-    lost: false
+    lost: false,
+    ngPlus: 0,
+    ngPlusButton: false
   },
   methods: {
-    generate: function() {
+    generate: function () {
       var number = Math.floor(Math.random() * 9);
+      number = 0;
       for (var i = 0; i < 3; i++) {
         Vue.set(this.mines, i, [1, 1, 1]);
         Vue.set(this.clicked, i, [0, 0, 0]);
       }
       Vue.set(this.mines[Math.floor(number / 3)], number % 3, -1);
+      this.ngPlusButton = false;
+    },
+    reset: function () {
+      this.generate();
       this.score = 0;
       this.lost = false;
+      this.ngplus = 0;
     },
-    getPic: function(index1, index2) {
-      if(this.clicked[index1][index2] === 0){
+    getPic: function (index1, index2) {
+      if (this.clicked[index1][index2] === 0) {
         return "button.png";
       } else {
-        if(this.mines[index1][index2] === 1){
+        if (this.mines[index1][index2] === 1) {
           return "shard.png";
         } else {
           return "quarble.png";
         }
       }
     },
-    trigger: function(index1, index2) {
+    trigger: function (index1, index2) {
       if (!this.lost) {
-        if(this.mines[index1][index2] === 1 && this.clicked[index1][index2] === 0) {
+        if (this.mines[index1][index2] === 1 && this.clicked[index1][index2] === 0) {
           this.score++;
-        } else if(this.mines[index1][index2] === -1) {
+          if (this.score % 8 == 0) {
+            this.ngPlus++;
+            this.ngPlusButton = true;
+          }
+        } else if (this.mines[index1][index2] === -1) {
           this.lost = true;
         }
         Vue.set(this.clicked[index1], index2, 1);
